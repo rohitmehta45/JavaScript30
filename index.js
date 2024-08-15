56191bd5a651321f3a8488ade413fe81d626dc97
Rohit <mehtarohit1012@gmailcom>
2024-08-15 19:19:56 +0545

    Html added

---
+++ index.js
@@ -0,0 +1,31 @@
+
+// let firstname="Rohit";
+// let lastname="Mehta";
+// let address="Inaruwa";
+// const phonenumbers="9817927648";
+// {
+//      var firstnames="Rohit";
+//      let lastnames="Mehta";
+//     console.log(firstnames);
+//     console.log(lastnames);
+// }
+// console.log(lastnames)
+// console.table([firstname,lastname]);
+// console.log(typeof undefined);
+// console.log(typeof null);
+let youtube="Mr. Beast";
+let anothername="DRSOK gaming";
+anothername="deadpool";
+console.log(youtube);
+console.log(anothername);
+let user1={
+    email:"test@gmail.com",
+    password:"13w3536",
+
+};
+let user2 = user1;
+user2.email= "changed Email from user2";
+user1.password="changed Password from user2";
+console.log('value of user2',user2);
+console.log('value of user1',user1);
+
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+console.log(lastnames)
+// console.table([firstname,lastname]);
+// console.log(typeof undefined);
+// console.log(typeof null);
